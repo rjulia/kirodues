63c84715b1cd6a19224bca624a17be13670eb624
Ramon Julia <ramon@tofugear.com>
2020-04-07 11:05:14 +0800

    [home] add menu

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,10 +1,60 @@
 import React from 'react'
+import './Header.scss'
+import Logo from "../assets/images/kirodues_log-300x113.jpg";
+import { NavLink as Link, useLocation, } from "react-router-dom";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+
 
 const Header = () => {
   return (
-    <div>
+    <nav className="header__container container-fluid">
 
-    </div>
+      <div className="header__logo">
+        <Link
+          exact to="/">    <img src={Logo} alt="" />
+        </Link>
+
+      </div>
+      <div className="header__menu--box">
+        <div className="header_menu--menu">
+          <ul>
+            <li>
+              <Link to="/nosotros">
+                About us
+              </Link>
+            </li>
+            <li>
+              <Link to="/tratamientos">
+                Tatamientos
+              </Link>
+            </li>
+            <li>
+              <Link to="/colaboradores">
+                colaboradores
+              </Link>
+            </li>
+            <li>
+              <Link to="/colaboradores">
+                Contacto
+              </Link>
+            </li>
+
+          </ul>
+        </div>
+        <div className="header__menu--language">
+          <span>ES</span> | <span>EN</span>
+        </div>
+        <div className="header__menu--social-media">
+          <ul>
+            <li>
+              <a href=""><FaFacebookF size={15} /></a>
+              <a href=""><FaTwitter size={15} /></a>
+              <a href=""><FaInstagram size={15} /></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
