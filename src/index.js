b5ecfc7696f23add1f7e391b50469099bfbce36c
Ramon Julia <ramon@tofugear.com>
2020-04-07 09:51:42 +0800

    add packs

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n/i18n";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <I18nextProvider i18n={i18n}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </I18nextProvider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
