8f6bc7e7df8ef3cce8f70b7e59a0e62e3da8a4cb
Ramon Julia <ramon@tofugear.com>
2020-04-11 11:10:29 +0800

    fix bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ export { default as AboutUs } from './AboutUs';
 export { default as Collaborator } from './Collaborator';
 export { default as Collaborators } from './Collaborators';
 export { default as Contact } from './Contact';
-export { default as Treatment } from './Treatment';
-export { default as Treatments } from './Treatments';
 export { default as NoFound } from './NoFound';
 export { default as Packs } from './Packs';
+export { default as Treatment } from './Treatment';
+export { default as Treatments } from './Treatments';
 
 
 
