7615a65ccbdab1a36754779d2e7d27b017b70102
Ramon Julia <ramon@tofugear.com>
2020-05-23 16:27:10 +0800

    fix bug in the route create page 404

--- src/pages/NoFound/index.js
+++ src/pages/NoFound/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./NoFound.scss";
+import { Title, Button } from "components";
+import { Link } from "react-router-dom";
 
 const NoFound = () => {
   return (
-    <div>
-      No found
+    <div className="no-found__container">
+      <h1>404</h1>
+      <Title tag={"h2"} dark text={"There's nothing here ..."} />
+      <p>.. maybe the page you're looking is not found or never exit</p>
+      <Link to="/">
+        <Button text={"go home"} />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default NoFound
+export default NoFound;
--- src/pages/Treatments/index.js
+++ src/pages/Treatments/index.js
@@ -1,51 +1,53 @@
-import _ from 'lodash';
-import React, { useState, useEffect } from 'react';
-import { Helmet } from 'react-helmet';
-import { useTranslation } from 'react-i18next';
-import './treatments.scss';
-import { Spinner, BlockTitle, BoxImage } from 'components';
-import { getTreatments } from '../../services/contenful';
+import _ from "lodash";
+import React, { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
+import { useTranslation } from "react-i18next";
+import "./treatments.scss";
+import { Spinner, BlockTitle, BoxImage } from "components";
+import { getTreatments } from "../../services/contenful";
 
 const Treatments = () => {
-
   const { t, i18n } = useTranslation();
-  const [isLoading, setLoading] = useState(true)
-  const [treatments, setTreatments] = useState([])
+  const [isLoading, setLoading] = useState(true);
+  const [treatments, setTreatments] = useState([]);
 
   useEffect(() => {
-    const promise = getTreatments(i18n.language)
-    setLoading(true)
+    const promise = getTreatments(i18n.language);
+    setLoading(true);
     promise
-      .then(data => {
-        setTreatments(data)
-      }).finally(() => {
-        setLoading(false)
+      .then((data) => {
+        setTreatments(data);
       })
-  }, [i18n.language])
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [i18n.language]);
 
-  if (isLoading) return <Spinner />
+  if (isLoading) return <Spinner />;
   return (
     <div className="treatments">
       <Helmet>
-        <title>{t('helmet.treatments.title')}</title>
-        <meta name="description" content={t('helmet.treatments.description')} />
+        <title>{t("helmet.treatments.title")}</title>
+        <meta name="description" content={t("helmet.treatments.description")} />
       </Helmet>
-      <BlockTitle title={t('discount_pack.title')} subtitle={t('discount_pack.content')} />
+      <BlockTitle
+        title={t("discount_pack.title")}
+        subtitle={t("discount_pack.content")}
+      />
       <div className="container">
         <div className="treatments__grid">
-          {
-            treatments.map(treatment => (
-              <BoxImage
-                key={treatment.sys.id}
-                name={_.get(treatment, 'fields.sortTreatment')}
-                slug={`tratamientos/${_.get(treatment, 'fields.slug')}`}
-                thumbnail={_.get(treatment, 'fields.thumbnail.fields')}
-              />))
-          }
+          {treatments.map((treatment) => (
+            <BoxImage
+              key={treatment.sys.id}
+              name={_.get(treatment, "fields.sortTreatment")}
+              slug={`/tratamientos/${_.get(treatment, "fields.slug")}`}
+              thumbnail={_.get(treatment, "fields.thumbnail.fields")}
+            />
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Treatments
+export default Treatments;
